2b9805a67da03e47272768298c76043540029f18
Taylor Bradshaw <taylordyoung1@gmail.com>
2020-03-16 17:16:33 -0400

    editied styles commented out feedback code for now

--- src/routes/LearningRoute/LearningRoute.js
+++ src/routes/LearningRoute/LearningRoute.js
@@ -27,18 +27,18 @@ class LearningRoute extends Component {
         </div>
         </div>
             {/* conditionally render feedback based on when user submits an answer and whether the response is correct or not */}
-        <div className="lr-feedback">
-            {/* components needed for positive and negative feedback */}
+        {/* <div className="lr-feedback">
+             components needed for positive and negative feedback
             <h3 className="lr-negativeMessage">Good try, but not quite right.</h3>
-            {/* dynamically inserted word, translation, translation attempt */}
+             dynamically inserted word, translation, translation attempt 
             <p className="lr-correctTranslation">The correct translation for Si us plau was Please and you chose Thank You.</p>
             <Button className="lr-nextButton">Try another word!</Button>
 
             <h3 className="lr-positiveMessage">Correct!</h3>
             <p className="lr-correctTranslation">The correct translation for Si us plau was Please and you chose Please.</p>
-            {/* handleNextWordClick function */}
+            handleNextWordClick function 
             <Button className="lr-nextButton">Try another word!</Button>
-        </div>
+        </div> */}
       </section>
     );
   }
