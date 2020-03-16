6cfd488ed8aafcf7656220ab604941c2bf2288f2
Jordan Castillo <jordanxcallaway@gmail.com>
2020-03-16 09:09:35 -0700

    starting dashboard

--- src/routes/DashboardRoute/DashboardRoute.js
+++ src/routes/DashboardRoute/DashboardRoute.js
@@ -1,9 +1,24 @@
 import React, { Component } from 'react'
+import UserContext from '../../contexts/UserContext'
 
 class DashboardRoute extends Component {
+  static contextType = UserContext;
   render() {
     return (
-      <section>
+      <section className='Dashboard'>
+        <header className='Dashboard_welcome'>
+          Welcome, {this.context.user.name}!
+        </header>
+        <div className='Dashboard_language'>
+          You're learning: Catalan
+        </div>
+        <div className='Dashboard_history'>
+          Brief history of language
+        </div>
+
+        <div className='Dashboard_table'>
+
+        </div>
         implement and style me
       </section>
     );
